47da3a44d65df17c1b5de1bf175dc9a46b3cdaa0
gerardcebria <root@LAPTOP-1TB3NPGG.localdomain>
2022-12-09 13:47:05 +0100

    gitignored

--- createtables/forms.py
+++ createtables/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class dataBaseForm(forms.Form):
     # server = forms.CharField(initial="whatever you want")
@@ -6,6 +8,13 @@ class dataBaseForm(forms.Form):
     # username = forms.CharField()
     # password = forms.CharField()
     # driver= forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_action = reverse_lazy('index')
+        self.helper.form_method = 'GET'
+        self.helper.add_input(Submit('submit', 'Submit'))
 
     SERVER_CHOICES = {
         (1, 'Triumph'),
